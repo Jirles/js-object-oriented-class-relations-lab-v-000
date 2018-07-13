f52b8992322423238ca0b222c604d54f223f4b4f
Emily Jirles <emily.jirles@gmail.com>
2018-07-13 17:02:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Driver {
     return store.trips.filter(trip => trip.driverId === this.id)
   };
   passengers(){
-    return this.trips()
+    return this.trips().reduce(passenger, [])
   };
 };
 
